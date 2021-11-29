01c06241b9a1db4035e5e611005e2b924b0a489f
叶伟 <302572771@qq.com>
2021-11-29 17:45:09 +0800

    初始化项目

--- Imooc_selenium/start_browser.py
+++ Imooc_selenium/start_browser.py
@@ -2,34 +2,48 @@
 from selenium import webdriver
 import time
 import random
-import pytesseract
+# import pytesseract
 from PIL import Image
-from ShowapiRequest import ShowapiRequest
+# from ShowapiRequest import ShowapiRequest
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
-driver = webdriver.Chrome()
+
+options=webdriver.ChromeOptions()
+
+# 忽略无用的日志
+options.add_experimental_option("excludeSwitches", ['enable-automation', 'enable-logging'])
+driver=webdriver.Chrome(options=options)
+
 #driver2 = webdriver.Firefox()
 #driver1 = webdriver.Edge()
 driver.get("http://www.5itest.cn/register")
-driver.maximize_window()
-time.sleep(5)
-print(EC.title_contains("注册"))
+# driver.maximize_window()
+# time.sleep(5)
+# print(EC.title_contains("注册"))
+
+
 email_element = driver.find_element_by_id("register_email")
 email_element.send_keys('sdfsd')
-driver.find_element_by_id('captcha_code').send_keys("test")
-print(driver.find_element_by_id('register_email-error').text)
-print(driver.find_element_by_id('register_email-error').get_attribute('value'))
-#driver.save_screenshot("E:/Teacher/Imooc/imooc2.png")
-#code_element = driver.find_element_by_id("getcode_num")
-#print(code_element.location)#{"x":123,"y":345}
-#left = code_element.location['x']
-#top = code_element.location['y']
-#right = code_element.size['width']+left
-#height = code_element.size['height']+top
-#im = Image.open("E:/Teacher/Imooc/imooc2.png")
-#img = im.crop((left,top,right,height))
-#img.save("E:/Teacher/Imooc/imooc3.png")
+# driver.find_element_by_id('captcha_code').send_keys("test")
+# print(driver.find_element_by_id('register_email-error').text)
+# print(driver.find_element_by_id('register_email-error').get_attribute('value'))
+
+
+driver.save_screenshot("D:/work_space/selenuim/image/imooc1.png")
+code_element = driver.find_element_by_id("getcode_num")
+print(code_element.location)#{"x":123,"y":345}
+left = code_element.location['x']
+top = code_element.location['y']
+left1 = 683
+top1 = 699
+right = code_element.size['width']+left1
+height = code_element.size['height']+top1
+im = Image.open("D:/work_space/selenuim/image/imooc1.png")
+img = im.crop((left,top,right,height))
+img.save("D:/work_space/selenuim/image/imooc2.png")
+
+
 
 #r = ShowapiRequest("http://route.showapi.com/184-4","62626","d61950be50dc4dbd9969f741b8e730f5" )
 #r.addBodyPara("typeId", "35")
@@ -43,21 +57,21 @@ print(driver.find_element_by_id('register_email-error').get_attribute('value'))
 
 
 
-#user_email = ''.join(random.sample('1234567890abcdefg',8))+"@163.com"
-#print(user_email)
+# user_email = ''.join(random.sample('1234567890abcdefg',8))+"@163.com"
+# print(user_email)
 
 
 
 
-element = driver.find_element_by_class_name("controls")
-locator = (By.CLASS_NAME,"controls")
-WebDriverWait(driver,1).until(EC.visibility_of_element_located(locator))
+# # element = driver.find_element_by_class_name("controls")
+# locator = (By.CLASS_NAME,"controls")
+# WebDriverWait(driver,1).until(EC.visibility_of_element_located(locator))
 
-print(email_element.get_attribute("placeholder"))
-email_element.send_keys("test@163.com")
-print(email_element.get_attribute("value"))
-time.sleep(5)
-driver.close()
+# print(email_element.get_attribute("placeholder"))
+# email_element.send_keys("test@163.com")
+# print(email_element.get_attribute("value"))
+# time.sleep(5)
+# driver.close()
 
 
 
@@ -67,3 +81,6 @@ driver.close()
 #user_element.send_keys("qerqewre")
 #driver.find_element_by_name("password").send_keys("111111")
 #driver.find_element_by_xpath("//*[@id='captcha_code']").send_keys("111111")
+
+
+driver.close()
